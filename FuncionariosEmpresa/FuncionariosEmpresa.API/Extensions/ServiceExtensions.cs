96551ff0d02ab50095baaf8d72fc7b05e4318289
danilo.silva <danilo.silva@msn.com>
2024-05-24 22:50:08 -0300

    Métodos de extensão e configuração de CORS

---
+++ FuncionariosEmpresa/FuncionariosEmpresa.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace FuncionariosEmpresa.API.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigurarCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+        });
+    }
+}
